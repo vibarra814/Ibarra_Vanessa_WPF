05103169bbb81bdaa185590e7f2fe6860b6b9683
vibarra814 <vibarra814@fullsail.edu>
2014-02-12 16:29:11 -0800

    adding variable a and b (expressions)

--- associatingJS/JS/script.js
+++ associatingJS/JS/script.js
@@ -1,11 +1,6 @@
-// Arrays
+// Expressions
 
-var muppetNames =["kermit", "miss piggy", "gonzo", "rizzo"]; //0,1,2,3
-
-
-muppetNames[1] = "rizzo"; 
-muppetNames[2]="fozzy";
-var num = 1;
-
-console.log(muppetNames[1]); //lets me see whats in the index #1
-
+var a = 2;
+var b;
+b = a+ 3; 
+console.log(b);
