fe9db603825ce5b8c1a798127212db8038960574
vibarra814 <vibarra814@fullsail.edu>
2014-02-12 17:51:36 -0800

    number of slices per person

--- associatingJS/JS/script.js
+++ associatingJS/JS/script.js
@@ -1,6 +1,8 @@
 // Vanessa Ibarra 2/12/2014 expression worksheet
 
-var age = 1; //sparkys age in human years
-result = age * 7; //calculation to figure dog years
-console.log(result) //printing result 
+var slices = 8; 
+var people = 4;
+var pizzas = 2;
+result = slices * pizzas /people;
+console.log (result); 
 
