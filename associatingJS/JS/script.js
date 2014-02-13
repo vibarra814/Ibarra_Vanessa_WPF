3ea72c80e4e9191db6070e070f19264e7ab7263f
vibarra814 <vibarra814@fullsail.edu>
2014-02-12 19:08:46 -0800

    using strings in prompts and alerts

--- associatingJS/JS/script.js
+++ associatingJS/JS/script.js
@@ -1,10 +1,8 @@
-// Vanessa Ibarra 2/12/2014 expression worksheet
+//promt and alert
+var width = prompt("we are calculating the area of a rectangle \nplease enter the width");
+var height = prompt ("please enter the height");
+
+var area = width * height;
+alert("the area of your rectangle is" + " " + area);
 
 
-var priceTotal = 100.00; //original price
-var discount = 25; //discount amount
-var priceTax = priceTotal + 10; //tax amount
- 
-var resultTax = priceTax - discount;// total with tax and discount
-var result = priceTotal - discount;// total with only discount
-console.log(result, resultTax); //printing both totals in console
