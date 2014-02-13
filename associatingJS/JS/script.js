025b7b323ff64b710a76e105460a399461760ec4
vibarra814 <vibarra814@fullsail.edu>
2014-02-12 18:04:32 -0800

    number of pizzas  slices sparky gets

--- associatingJS/JS/script.js
+++ associatingJS/JS/script.js
@@ -1,8 +1,9 @@
 // Vanessa Ibarra 2/12/2014 expression worksheet
 
-var slices = 8; 
-var people = 4;
-var pizzas = 2;
-result = slices * pizzas /people;
-console.log (result); 
+
+var slices = 8; //number of slices per pizza
+var people = 4; //number of people at party
+var pizzas = 2; //number of pizzas
+var remainder = 16 % 16; //leftover after everyone gets there share
+console.log(remainder); // printed onto console
 
