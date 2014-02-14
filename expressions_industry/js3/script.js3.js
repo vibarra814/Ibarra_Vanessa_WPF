f1942af6ff6991364f7c9383d14c611cbae2cca5
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 18:41:11 -0800

    corrected result alert

--- expressions_industry/js3/script.js3.js
+++ expressions_industry/js3/script.js3.js
@@ -5,12 +5,13 @@ var pixelHeight = prompt(" I am calculating your images are in pixels. Please en
 
 var pixelWidth = prompt("please enter your images pixel width");
 
-var pixelresult = "your are in pixels is" + " " + pixelHeight * pixelWidth;
+var pixelresult = "your area in pixels is" + " " + pixelHeight * pixelWidth;
  
 alert (pixelresult);
 
 var pixelHeight2 = prompt("Now we will calculate the area in inches. Please enter your pixel height again.");
 var resolutionHeight = prompt("please enter your computer screens resolution height");
  
-  inchesHeightresult = "your images height in inches is" + " " + Number"pixelHeight2" / Number"resolutionHeight";
+ var inchesHeightresult = "your images height in inches is" + " " + resolutionHeight / pixelHeight2;
+  
   alert (inchesHeightresult);
