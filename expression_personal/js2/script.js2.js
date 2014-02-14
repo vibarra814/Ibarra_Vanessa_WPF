0a0f5b203e2a0a0ef6305d5016df2a85afbbece8
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 17:45:48 -0800

    added comment to line 3

--- expression_personal/js2/script.js2.js
+++ expression_personal/js2/script.js2.js
@@ -2,8 +2,8 @@
 
 
 
-var weekly = prompt("I am calculating yearly income for 2013.Please enter weekly salary");
-var yearly = prompt("please enter number of weeks in 2013");
+var weekly = prompt("I am calculating yearly income for 2013.Please enter weekly salary");//added the prompt to input eekly salary
+var yearly = prompt("please enter number of weeks in 2013"); //
 var extras = prompt("please enter any extra earnings");
 
 var result = weekly * yearly + Number(extras);
