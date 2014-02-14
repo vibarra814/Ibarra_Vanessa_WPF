dbdf34e6fbe5084378ba9453d736147a97082cd2
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 18:21:01 -0800

    added space in pixel result string

--- expressions_industry/js3/script.js3.js
+++ expressions_industry/js3/script.js3.js
@@ -5,6 +5,6 @@ var pixelHeight = prompt("please enter your images pixel height");
 
 var pixelWidth = prompt("please enter your images pixel width");
 
-var pixelresult = "your pixel area is" + pixelHeight * pixelWidth;
+var pixelresult = "your pixel area is" + " " + pixelHeight * pixelWidth;
  
 alert (pixelresult);
