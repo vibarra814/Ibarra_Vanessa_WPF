646b82b65701f0ae893b6411768f4b216791da28
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 17:00:17 -0800

    redo adding prompts as variables.

--- expression_personal/js/script.js
+++ expression_personal/js/script.js
@@ -1,11 +1,9 @@
 //Vanessa Ibarra 2/13/2014 expressions my daughters yearly allowance
-var daily = 1; // establishing the amount of daily allowance
 
-var weekly = daily * 7; // giving an equation to solve for wekly allowance
 
-var monthly = weekly * 4;// giving equation to solve fro monthly allowance
 
-var result = monthly * 12;//giving the final equation to get result
+var daily= prompt(please enter daily allowance);
 
-console.log(result);// orinting out to console 
+
+console.log(result);// printing out to console 
 
