054bdd6bb5c1b2b87c75304c419ab9f70744f506
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 20:34:43 -0800

    added () and Number to line 11

--- expressions_wacky/js4/script.js4.js
+++ expressions_wacky/js4/script.js4.js
@@ -8,6 +8,6 @@ var calBurned = prompt("please enter calories burned today"); // added prompt to
  
 var cheatCal = "The amount of daily cheat calories you have today are" + " " + Number(calPerday) - Number(calToday); //added equation to figure out amount of cheat calories left
  
-cheatCal += calBurned; //added equation to figure cheat calories plus calories burned
+cheatCal += Number(calBurned); //added equation to figure cheat calories plus calories burned
 
  console.log(cheatCal); // added the answer for total calories left to console (did not work) 
