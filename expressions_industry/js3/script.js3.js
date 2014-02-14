7e43c26279b18578135f3f37acbe9ace9d5504a8
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 18:27:34 -0800

    added inches result as alert

--- expressions_industry/js3/script.js3.js
+++ expressions_industry/js3/script.js3.js
@@ -1,7 +1,7 @@
 //Vanessa Ibarra 2/3/2014 expressions calculating pixel/inches of an image
 
 
-var pixelHeight = prompt(" I am calculating your images are in pixels. /n please enter your images pixel height");
+var pixelHeight = prompt(" I am calculating your images are in pixels. Please enter your images pixel height");
 
 var pixelWidth = prompt("please enter your images pixel width");
 
@@ -9,4 +9,10 @@ var pixelresult = "your pixel area is" + " " + pixelHeight * pixelWidth;
  
 alert (pixelresult);
 
-var inchesHeight = prompt(" Now we will find the area in inches. /n please enter your images height in inches")
+var inchesHeight = prompt(" Now we will find the area in inches. Please enter your images height in inches");
+ 
+ var inchesWidth = prompt("please enter your images width in inches");
+
+ var inchesresult = "your inches area is" + " " + inchesWidth * inchesHeight;
+
+ alert (inchesresult);
