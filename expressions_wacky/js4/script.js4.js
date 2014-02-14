e07381ddc1aaca340a1891f2e8bdc7a09663da4c
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 21:34:58 -0800

    fixed mistake (just needed a coding break)

--- expressions_wacky/js4/script.js4.js
+++ expressions_wacky/js4/script.js4.js
@@ -1,13 +1,13 @@
 //Vanessa Ibarra 2/13/2014 expressions wacky
 
-var calPerday = prompt("I am calculating the amount of cheat calories you have left today. \nPlease enter the amount of calories you are allowed daily");//added prompt to input amount of calories allowed a day
+var calPerday = prompt("I am calculating the amount of cheat calories you have left today. \nPlease enter the amount of calories you are allowed daily");
 
-var calToday = prompt("Please enter number of calories you consumed today"); // added prompt to input number calories eaten
+var calToday = prompt("Please enter number of calories you consumed today"); 
 
-var calBurned = prompt("please enter calories burned today"); // added prompt to input number of calories burned
+var calBurned = prompt("please enter calories burned today"); 
  
-var cheatCal = "The amount of daily cheat calories you have today are" + " " + Number(calPerday) - Number(calToday); //added equation to figure out amount of cheat calories left (possible mistake in this line)
- 
-cheatCal += Number(calBurned); //added equation to figure cheat calories plus calories burned (possible mistake in this line)
+var cheatCal =  Number(calPerday) - Number(calToday); 
 
- console.log(cheatCal); // added the answer for total calories left to console (did not work) (possible mistake in this line?????????help)
+cheatCal += Number(calBurned); 
+ 
+alert("you have" + " " + (cheatCal) + " " + "left today."); 
