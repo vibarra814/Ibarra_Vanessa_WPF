af3496bbaba889137daf6d57466e35e661f4a39c
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 17:13:15 -0800

    added ; where neglected

---
+++ expression_personal/script.js
@@ -0,0 +1,12 @@
+//Vanessa Ibarra 2/13/2014 expressions my yearly income
+
+
+
+var weekly = prompt("I am calculating yearly income for 2013.Please enter weekly salary");
+var yearly = prompt("please enter number of weeks in 2013");
+var extras = prompt("please enter any extra earnings");
+
+var result = weekly * yearly + extras;
+
+console.log(result); // printing out to console 
+
