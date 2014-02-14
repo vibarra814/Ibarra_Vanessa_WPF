5452e99a698a60c73fcd480471c5b781893e34a3
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 16:46:57 -0800

    added comments

--- expression_personal/js/script.js
+++ expression_personal/js/script.js
@@ -1,11 +1,11 @@
 //Vanessa Ibarra 2/13/2014 expressions my daughters yearly allowance
-var daily = 1.00; 
+var daily = 1; // establishing the amount of daily allowance
 
-var weekly = daily * 7;
+var weekly = daily * 7; // giving an equation to solve for wekly allowance
 
-var monthly = weekly * 4;
+var monthly = weekly * 4;// giving equation to solve fro monthly allowance
 
-var result = monthly * 12;
+var result = monthly * 12;//giving the final equation to get result
 
-console.log(result);
+console.log(result);// orinting out to console 
 
