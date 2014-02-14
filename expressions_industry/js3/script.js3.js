74f047ffd84fee6bbfd73fd0c0a9470f49591ffb
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 18:22:39 -0800

    added new promt for inches height input

--- expressions_industry/js3/script.js3.js
+++ expressions_industry/js3/script.js3.js
@@ -8,3 +8,5 @@ var pixelWidth = prompt("please enter your images pixel width");
 var pixelresult = "your pixel area is" + " " + pixelHeight * pixelWidth;
  
 alert (pixelresult);
+
+var inchesHeight = prompt(" Now we will find the area in inches. /n please enter your images height in inches")
