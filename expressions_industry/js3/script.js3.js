e5a7e60dbd9b49f8584416c947da0ee1da2c4b1f
vibarra814 <vibarra814@fullsail.edu>
2014-02-13 18:13:34 -0800

    added width/ height prompts

--- expressions_industry/js3/script.js3.js
+++ expressions_industry/js3/script.js3.js
@@ -1,4 +1,8 @@
-//Vanessa Ibarra 2/3/2014 expressions industry
+//Vanessa Ibarra 2/3/2014 expressions calculating pixel/inches of an image
 
-var width =
-var height = 
+
+var pixelHeight = prompt("please enter your images pixel height");
+
+var pixelWidth = prompt("please enter your images pixel width");
+
+var pixelresult = pixelHeight * pixelWidth
