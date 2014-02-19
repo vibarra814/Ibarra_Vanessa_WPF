6ddb21e51a3cede8d1509e63fa36264eeadc4c1c
vibarra814 <vibarra814@fullsail.edu>
2014-02-19 14:42:19 -0800

    added comments to every line

--- conditionals_industry/js6/script.js
+++ conditionals_industry/js6/script.js
@@ -1,10 +1,10 @@
 //Vanessa Ibarra 2/19/2014 assignment conditionals: industry
 
-var langPref = prompt("I am deciding on new pocket refrence guides for our office. \n please input whether you prefer javascript or VBscript.");
- 	if (langPref === "javascript") {
-	console.log ("thanks. your vote for the javascript pocket guide has been  entered");
-	} else if(langPref === ("") ){prompt("you forgot to enter a prefrence");
+var langPref = prompt("I am deciding on new pocket refrence guides for our office. \n please input whether you prefer javascript or VBscript.");//created variable for a prompt for users to input their lang. pref.
+ 	if (langPref === "javascript") {// added equation if they choose java 
+	console.log ("thanks. your vote for the javascript pocket guide has been  entered");// if java is chosen console will print this 
+	} else if(langPref === ("") ){prompt("you forgot to enter a prefrence");// added prompt to show in case nothing is entered
 	}
  	else {
- 	console.log ("thanks. your vote for the VBscript pocket guide has been entered");
+ 	console.log ("thanks. your vote for the VBscript pocket guide has been entered"); //entered equation to solve  if java is not picked but something is entered
  	}
