80387ad2ff2ad2416ac10864d4d7064cb503bfe7
vibarra814 <vibarra814@fullsail.edu>
2014-02-19 15:04:59 -0800

    changed prompt to instruct user to reresh page

--- conditionals_wacky/js8/script.js
+++ conditionals_wacky/js8/script.js
@@ -7,6 +7,6 @@ var show;
 // added ternary to give one recomendation to all over 16 and one to all under 16
 show = (age < 16) ? "I recomenmend you watch steven universe" : "I recommend you watch rick and morty";
 //added a prompt in case nothing is entered
-if (age === ""){prompt("you forgot to enter your age")}
+if (age === ""){prompt("you forgot to enter your age, please refresh page")}
 //print recomendation to console
 console.log(show);
