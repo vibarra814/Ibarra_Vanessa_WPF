5b68956166ed30cb98d03d45a69f2b8160100f06
vibarra814 <vibarra814@fullsail.edu>
2014-02-18 17:23:40 -0800

    added else if to roller coaster problem

--- associatingJS/JS/script.js
+++ associatingJS/JS/script.js
@@ -1,11 +1,13 @@
-//conditional logic
+//conditional logic - else if
 
 var kidHeight = 48;
 var minHeight = 48;
-
+var wParentHeight = 45;
 
 if(kidHeight  > minHeight){
 	console.log("you can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+	console.log("you can ride, but only with a parent");
 }else{
 	console.log("sorry you've got some growing to do!");
 }
