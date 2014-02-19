3e604932c767658e6f66a883af47dbf6922aa992
vibarra814 <vibarra814@fullsail.edu>
2014-02-18 18:02:41 -0800

    used the && and || operators in phone/budget problem

--- associatingJS/JS/script.js
+++ associatingJS/JS/script.js
@@ -1,11 +1,11 @@
 //conditional logic - operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var payCheck = 200;
+var wonLottery = true ;
 
 // if the price of our phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && payCheck > 300){ 
+if(iPhonePrice < budget || wonLottery ){ 
 	console.log("we can buy the phone");
 }else {
 	console.log("no phone for you!"); 
