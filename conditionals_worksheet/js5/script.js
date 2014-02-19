cbb8f5ea253ec53884dc1992e65781829e96fd54
vibarra814 <vibarra814@fullsail.edu>
2014-02-18 20:48:56 -0800

    tried to solve problem of weong answer printing to console : no luck

--- conditionals_worksheet/js5/script.js
+++ conditionals_worksheet/js5/script.js
@@ -1,12 +1,13 @@
 // vanessa Ibarra 2/18/2014 activity conditionals worksheet
 // movie ticket price
 
-var movieTime = 11
-var age = 30
+var movieTime = 2
+var age = 15
 
-if (movieTime = 3, 4, 5 || age <= 10 || age >=55){
-
+if (movieTime === 3 || 4 || 5){
 	console.log("your price is $7.00");
-}else {
+} else if(age < 10 || age > 55){
+	console.log("your price is $7.00");
+} else{
 	console.log("your price is $12.00");
 }
