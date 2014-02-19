cf3d0ab98ad91789d88f60ecd9054f73f5c65bf8
vibarra814 <vibarra814@fullsail.edu>
2014-02-19 15:00:34 -0800

    added prompt in case nothing is entered

--- conditionals_wacky/js8/script.js
+++ conditionals_wacky/js8/script.js
@@ -2,7 +2,11 @@
 
 
 var age = prompt("I am recommending cartoons to people. \n Please enter your age here. ");
+
+
 var show; 
 
 show = (age < 16) ? "I recomenmend you watch steven universe" : "I recommend you watch rick and morty";
+
+if (age === ""){prompt("you forgot to enter your age")}
 console.log(show);
