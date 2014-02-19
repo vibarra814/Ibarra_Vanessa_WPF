bd54de372ee055c813031f7b804c021e37a224fd
vibarra814 <vibarra814@fullsail.edu>
2014-02-18 19:22:59 -0800

    solved check the login problem

--- conditionals_worksheet/js5/script.js
+++ conditionals_worksheet/js5/script.js
@@ -1,13 +1,13 @@
 // vanessa Ibarra 2/18/2014 activity conditionals worksheet
-// last chance for gas
+// check the login 
 
-var mpg = 35;
-var percentGas = 20;
-var capacity = 12;
-var gas = capacity * percentGas /100;
-
-if(mpg * gas >= 200){
-console.log("yes, you can make it with out stopping for gas!");
-}else{
-console.log("you only have" + " " +(gas)+ " " + "gallons of gas in your tank, better get gas now while you can!");
+var userName = prompt("please enter your user name");
+ 
+if (userName === "pollywoggle"){
+	var passWord = prompt("please enter password");
+}else {alert ("you have entered an invalid username");
 }
+
+if (passWord === "giggle"){
+	console.log("welcome!");
+}else prompt("you have entered an invalid password");
