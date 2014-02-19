96beca607bda5378e49cf8fb957466d12a792e3e
vibarra814 <vibarra814@fullsail.edu>
2014-02-19 14:27:32 -0800

    started language prefrence guide code

---
+++ conditionals_industry/js6/script.js
@@ -0,0 +1,10 @@
+//Vanessa Ibarra 2/19/2014 assignment conditionals: industry
+
+var langPref = prompt("I am deciding on new pocket refrence guides for our office. \n please input whether you prefer javascript or VBscript.");
+ 	if (langPref = javascript) {
+ 	console.log ("thanks. your vote for the javascript pocket guide has been  entered");
+	} if(langPref = ("") ){prompt("you forgot to enter a prefrence");
+	}
+ 	else {
+ 	console.log ("thanks. your vote for the VBscript pocket guide has been entered");
+ 	}
