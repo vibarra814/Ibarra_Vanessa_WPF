6d208ad061435e0193a5c2fb3ff842617533b63d
vibarra814 <vibarra814@fullsail.edu>
2014-02-19 14:57:23 -0800

    added recomendation for cartoons

--- conditionals_wacky/js8/script.js
+++ conditionals_wacky/js8/script.js
@@ -1,2 +1,8 @@
 // Vanessa Ibarra 2/19/2014  assignment conditionals:wacky
 
+
+var age = prompt("I am recommending cartoons to people. \n Please enter your age here. ");
+var show; 
+
+show = (age < 16) ? "I recomenmend you watch steven universe" : "I recommend you watch rick and morty";
+console.log(show);
