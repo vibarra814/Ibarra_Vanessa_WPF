ee4f50d862e4530347bd6154651591dd1a5b0e53
vibarra814 <vibarra814@fullsail.edu>
2014-02-24 18:40:19 -0800

    added comments to every line

--- functions_worksheet/js/script.js
+++ functions_worksheet/js/script.js
@@ -1,10 +1,11 @@
 //Vanessa Ibarra 2/24/2014 functions worksheet
 
-circumfrence(3.14, 8);
+// to figure out the circumfrence you need to multiply 2 by pie by the radius
+circumfrence(3.14, 8);// gave values to pie and radius
 
-function circumfrence(p, r){
-     var result = 2 * p * r;
-    console.log("The circumfrence of the circle is " + result + " inches.");
+function circumfrence(p, r){// made function to equate circumfrence
+     var result = 2 * p * r; // actual equation
+    console.log("The circumfrence of the circle is " + result + " inches.");// added sentence to print out into console with circumfrence result
 }
 
 
