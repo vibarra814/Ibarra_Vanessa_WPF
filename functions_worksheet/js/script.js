94775b75fdbcf24cdf02ac93e571b7c56e7cc9dd
vibarra814 <vibarra814@fullsail.edu>
2014-02-24 19:13:52 -0800

    fixed argument and parameters (switched)

--- functions_worksheet/js/script.js
+++ functions_worksheet/js/script.js
@@ -1,9 +1,9 @@
 //Vanessa Ibarra 2/24/2014 functions worksheet
 
-var total = stingstoKill(vicWieght, stingsPer)
+var total = stingstoKill(100, 8.666666667);
 
-function stingstoKill(100, 8.666666667){
+function stingstoKill(vicWieght, stingsPerlb){
 	
-    var stings = vicWieght * stingsPer
+    var stings = vicWieght * stingsPerlb;
 	return stings;
 }
