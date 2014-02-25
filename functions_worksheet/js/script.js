1dafa2f8e69403421fff05ef16e38b534491fe89
vibarra814 <vibarra814@fullsail.edu>
2014-02-24 19:20:10 -0800

    commented line 2

--- functions_worksheet/js/script.js
+++ functions_worksheet/js/script.js
@@ -1,5 +1,5 @@
 //Vanessa Ibarra 2/24/2014 functions worksheet
-
+// to figure out how many stings it takes to kill an animal multiply its weight by 8.666666667
 var total = stingstoKill(100, 8.666666667);
 
 function stingstoKill(vicWeight, stingsPerlb){
@@ -7,4 +7,4 @@ function stingstoKill(vicWeight, stingsPerlb){
     var stings = vicWeight * stingsPerlb;
 	return stings;
 }
-console.log("It takes " + total + " stings to kill an animal that weighs " + vicWeight +" pounds.");
+console.log("It takes " + total + " stings to kill this animal");
