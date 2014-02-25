bf030f5d8d20b8b1c3c8390003e7c6fc6f5c9e7d
vibarra814 <vibarra814@fullsail.edu>
2014-02-24 18:35:15 -0800

    added message to print to console

--- functions_worksheet/js/script.js
+++ functions_worksheet/js/script.js
@@ -1,9 +1,10 @@
 //Vanessa Ibarra 2/24/2014 functions worksheet
+
 circumfrence(3.14, 4);
 
 function circumfrence(pie, radius){
      var result = 2 * pie * radius;
-    console.log(result);
+    console.log("The circumfrence of the circle is " + result + " inches.");
 }
 
 circumfrence();
