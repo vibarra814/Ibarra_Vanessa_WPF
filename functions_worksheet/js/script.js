555871cdc0e6c20505ea2447c9e9b511ce6d02e3
vibarra814 <vibarra814@fullsail.edu>
2014-02-24 19:25:48 -0800

    changed vicWeight to 400 from 100 : equation still correct

--- functions_worksheet/js/script.js
+++ functions_worksheet/js/script.js
@@ -1,6 +1,6 @@
 //Vanessa Ibarra 2/24/2014 functions worksheet
 // to figure out how many stings it takes to kill an animal multiply its weight by 8.666666667
-var total = stingstoKill(100, 8.666666667);// created a variable for the total and added 100 and 8.666666667 as arguments
+var total = stingstoKill(400, 8.666666667);// created a variable for the total and added 100 and 8.666666667 as arguments
 
 function stingstoKill(vicWeight, stingsPerlb){ //created function to figure how many stings it will take to kill animal and added parameters
 	
