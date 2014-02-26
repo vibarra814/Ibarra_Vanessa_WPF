6c7d8091882f4f5696629f66a0ddcfab221738dc
vibarra814 <vibarra814@fullsail.edu>
2014-02-25 17:45:17 -0800

    added function for pixel width

--- functions_industry/js/script.js
+++ functions_industry/js/script.js
@@ -3,7 +3,14 @@
 var borderWidth = prompt("please enter your websites desired border width in inches");
 var featureWidth = prompt("please enter your websites desired feature box width in inches");
 var videoWidth = prompt("please enter your websites desired video width in inches");
- function 
+var width = pixelWidth(borderWidth, FeatureWidth, videoWidth);
+
+ function pixelWidth(borderWidth, featureWidth, videoWidth){
+ 	var pixelWidth = borderWidth + featureWidth + videoWidth;
+ 	return width;
+ };
+console.log("your websites total pixel width will be " + width);
+
 var borderHeight
 var featureHeight
 var videoHeight
