6b7f3f64a8cc87e4ad09799eeb1a7f296f4ccde3
vibarra814 <vibarra814@fullsail.edu>
2014-02-25 18:58:06 -0800

    fixed function and totals

--- functions_wacky/js/script.js
+++ functions_wacky/js/script.js
@@ -1,26 +1,26 @@
 //Vanessa Ibarra 2/25/2014 functions wacky
 
-var cowWeight = prompt("please enter the weight of your cow") 
-var lactation = Prompt("Is your cow lactating? please answer yes or no")
+var cowWeight = prompt("please enter the weight of your cow in pounds") 
+var lactation = prompt("Is your cow lactating? please answer yes or no")
 
 var feed = function(cowWeight, lactation){
 	var total = cowWeight * 0.25;
-      return feed;
+      return total;
 }
 
 var milk = function(cowWeight, lactation){
 	var total2 = cowWeight * 0.50;
-	return milk
+	return total2;
 }
 
 var total = feed(cowWeight, lactation);
 
 var total2 = milk(cowWeight, lactation);
 
-if(lactation === no){
+if(lactation === "no"){
 	console.log("your cow needs " + total + " pounds of feed a day");
 }
-else if (lactation === yes){
-	console.log("your cow needs " + total2 +)
+else if (lactation === "yes"){
+	console.log("your cow needs " + total2 +" pounds of food a day");
 }
 
