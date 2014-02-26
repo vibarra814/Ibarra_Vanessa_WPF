034fef13874f816f34218b7d128b86f45e5b245d
vibarra814 <vibarra814@fullsail.edu>
2014-02-25 18:46:40 -0800

    started cow feed calculator

---
+++ functions_wacky/js/script.js
@@ -0,0 +1,18 @@
+//Vanessa Ibarra 2/25/2014 functions wacky
+
+var cowWeight = prompt("please enter the weight of your cow") 
+var lactation = Prompt("Is your cow lactating? please answer yes or no")
+
+var feedWeight = function(cowWeight, lactation){
+	var feed = cowWeight * 0.25;
+      return total;
+}
+var total = feedWeight(cowWeight, lactation)
+
+if(lactation === no){
+	console.log("your cow needs " + total + " pounds of feed a day");
+}
+else if (lactation === yes){
+	console.log("your cow needs " + total2 +)
+}
+
