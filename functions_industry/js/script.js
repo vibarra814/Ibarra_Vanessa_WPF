4429e0261efe0ad782db7d80d7a500d04124f08f
vibarra814 <vibarra814@fullsail.edu>
2014-02-25 17:51:11 -0800

    added prompts for pixel heights/widths and console print out

--- functions_industry/js/script.js
+++ functions_industry/js/script.js
@@ -1,8 +1,8 @@
 //Vanessa Ibarra 2/25/2014 functions industry
 
-var borderWidth = prompt("please enter your websites desired border width in inches");
-var featureWidth = prompt("please enter your websites desired feature box width in inches");
-var videoWidth = prompt("please enter your websites desired video width in inches");
+var borderWidth = prompt("please enter your websites desired border width in pixels");
+var featureWidth = prompt("please enter your websites desired feature box width in pixels");
+var videoWidth = prompt("please enter your websites desired video width in pixels");
 var width = pixelWidth(borderWidth, FeatureWidth, videoWidth);
 
  function pixelWidth(borderWidth, featureWidth, videoWidth){
@@ -11,6 +11,14 @@ var width = pixelWidth(borderWidth, FeatureWidth, videoWidth);
  };
 console.log("your websites total pixel width will be " + width);
 
-var borderHeight
-var featureHeight
-var videoHeight
+var borderHeight = prompt("please enter your websites desired border height in pixels");
+var featureHeight = prompt("please eneter your websites desired feature box height in pixels");
+var videoHeight = prompt("please enter your websites desired video height in pixels");
+var height = pixelHeight(borderHeight, geatureHeight, videoHeight);
+
+function pixelHeight(borderHeight, geatureHeight, videoHeight){
+	varpixelHeight = borderHeight = featureHeight + videoHeight;
+	return height
+}
+console.log("your websites total pixel height will be " + height);
+}
