11482b60f8f8be3e75d5da8a5e43b3dfeba83816
vibarra814 <vibarra814@fullsail.edu>
2014-02-25 17:41:49 -0800

    started with variables for pixel size

---
+++ functions_industry/js/script.js
@@ -0,0 +1,9 @@
+//Vanessa Ibarra 2/25/2014 functions industry
+
+var borderWidth = prompt("please enter your websites desired border width in inches");
+var featureWidth = prompt("please enter your websites desired feature box width in inches");
+var videoWidth = prompt("please enter your websites desired video width in inches");
+ function 
+var borderHeight
+var featureHeight
+var videoHeight
