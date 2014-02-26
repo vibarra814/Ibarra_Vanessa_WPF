6e72e8850d6e563feeb512b674bc27a8991cd6da
vibarra814 <vibarra814@fullsail.edu>
2014-02-25 16:23:49 -0800

    created js file for personal function

---
+++ functions_personal/js/script.js
@@ -0,0 +1,18 @@
+//Vanessa Ibarra 2/25/2014 functions personal
+
+//i want to calculate  my BMI
+prompt("please enter your weight in pounds");
+
+prompt("please enter you height in inches");
+
+function bmi (weight, height){
+	var bmiHeight = height * height ;
+	var total = weight / bmiHeight * 703;
+	console.log("your body mass index is " + total);
+    
+}
+  if(total < 18){
+  	console.log("you are considered underweight.");
+  }
+  if(total = 19 || total = 20 || total = 21 )
+
